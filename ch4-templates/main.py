67f602496176707c2be5f34d9ddbeb4398c5cd92
Javier M. Piedragil Gálvez <javier.piedragil@gmail.com>
2024-01-21 13:24:38 -0600

    📦 NEW: Rendering basic HTML
    - Add code to display a basic HTML page.

---
+++ ch4-templates/main.py
@@ -0,0 +1,19 @@
+import fastapi
+import uvicorn
+
+app = fastapi.FastAPI()
+
+
+@app.get("/")
+def index():
+    content = """
+    
+    <h1>Hello FastAPI Web App</h1>
+    <div>This is where our fake pypi app will live!</div>
+    
+    """
+    return fastapi.responses.HTMLResponse(content)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
